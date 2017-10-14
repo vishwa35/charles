e02006952ef7c14645e74d0b59c154a9ed7aaac8
Vikram Krishnamurthy <vikramkrishnamurthy@lawn-143-215-56-53.lawn.gatech.edu>
2017-10-14 18:06:45 -0400

    html stuff basic af

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sentiment import Sentiment
 import json
 
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     s = Sentiment("Trump")
-    return json.dumps(s.get_tweets())
+    return render_template('index.html')
+    # return json.dumps(s.get_tweets())
 
 if __name__ == "__main__":
     app.run()
