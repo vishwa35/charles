d02d5a5ba5aa6412e38d1dda849db8fc157c82e6
Uday Patil <udpatil@gmail.com>
2017-10-14 16:20:48 -0400

    stuff

--- twscrape.py
+++ twscrape.py
@@ -1,2 +1,39 @@
 import tweepy
+from charles import getAPI
 
+class Scraper():
+
+	def __init__(self, keyword):
+		self.api = getAPI.getAPI()
+		self.topic = keyword
+
+	def get_tweets(self, count = 10):
+        '''
+        Main function to fetch tweets and parse them.
+        '''
+        # empty list to store parsed tweets
+        tweets = []
+ 
+        try:
+            # call twitter api to fetch tweets
+            fetched_tweets = self.api.search(q = self.topic, count = count)
+ 
+            # parsing tweets one by one
+            for tweet in fetched_tweets:
+                # empty dictionary to store required params of a tweet
+                parsed_tweet = (tweet.id, tweet.text, tweet.timestamp_ms)
+                
+                # appending parsed tweet to tweets list
+                if tweet.retweet_count > 0:
+                    # if tweet has retweets, ensure that it is appended only once
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+ 
+            # return parsed tweets
+            return tweets
+ 
+        except tweepy.TweepError as e:
+            # print error (if any)
+            print("Error : " + str(e))
