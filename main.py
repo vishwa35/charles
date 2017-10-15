2d4d64f5e57c274c6a56a5f6427051432ce04f04
Vikram Krishnamurthy <vikramkrishnamurthy@lawn-143-215-56-53.lawn.gatech.edu>
2017-10-15 00:48:18 -0400

    basic site ready, with user input and screen reset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from sentiment import TSentiment
 # from reddit import RSentiment
+
 import json
 import sqlite3
 
@@ -30,7 +31,8 @@ app = Flask(__name__)
 #     return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
-def home():
+def main():
+    print 'jjj'
     return render_template('index.html')
 
 @app.route("/sent_json")
@@ -38,8 +40,13 @@ def tweet_sent():
     s = TSentiment("Trump")
     return json.dumps(s.get_tweets())
 
+@app.route('/req', methods=['GET'])
+def my_form_get():
+    text =  request.args.get('text')
+    print(text)
+    return render_template('index.html')
+
 if __name__ == "__main__":
     # for user in query_db('select * from users'):
     #     print user['name']
     app.run()
-    # close_con nection()
