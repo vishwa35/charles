93396df6d18396b39efc2deee24a42101d70ddbe
Vishwa Shah <vshah0350@gmail.com>
2017-10-14 23:22:27 -0400

    try reddit?

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 from flask import Flask, render_template, g
-from sentiment import Sentiment
+from twitter import TSentiment
+from reddit import RSentiment
 import json
 import sqlite3
 
 app = Flask(__name__)
-DATABASE = 'charles.db'
+# DATABASE = 'charles.db'
 
-def connect():
-    with app.app_context():
-        return get_db()
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-def query_db(query, args=(), one=False):
-    cur = connect().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+# def connect():
+#     with app.app_context():
+#         return get_db()
+#
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#     return db
+#
+# @app.teardown_appcontext
+# def close_connection():
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
+#
+# def query_db(query, args=(), one=False):
+#     cur = connect().execute(query, args)
+#     rv = cur.fetchall()
+#     cur.close()
+#     return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def home():
@@ -34,13 +35,11 @@ def home():
 
 @app.route("/sent_json")
 def tweet_sent():
-    s = Sentiment("Trump")
+    s = TSentiment("Trump")
     return json.dumps(s.get_tweets())
 
 if __name__ == "__main__":
-
-    for user in query_db('select * from users'):
-        print user['name']
-
+    # for user in query_db('select * from users'):
+    #     print user['name']
     app.run()
     # close_con nection()
